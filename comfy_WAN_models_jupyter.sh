0ef7296fe2f3210fc4709ca860ab7610e44de9fb
ggboi25 <golugb96@gmail.com>
2025-03-20 20:48:14 +0530

    Update comfy_WAN_models_jupyter.sh

--- comfy_WAN_models_jupyter.sh
+++ comfy_WAN_models_jupyter.sh
@@ -3,7 +3,7 @@
 # Create necessary directories if they don't exist
 mkdir -p models/diffusion_models
 mkdir -p models/text_encoders
-mkdir -p models/VAE
+mkdir -p models/vae
 
 # Download diffusion model
 wget -O models/diffusion_models/Wan2_1-I2V-14B-480P_fp8_e4m3fn.safetensors \
@@ -17,7 +17,7 @@ wget -O models/text_encoders/umt5-xxl-enc-fp8_e4m3fn.safetensors \
     "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/umt5-xxl-enc-fp8_e4m3fn.safetensors"
 
 # Download VAE model
-wget -O models/VAE/Wan2_1_VAE_fp32.safetensors \
+wget -O models/vae/Wan2_1_VAE_fp32.safetensors \
     "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1_VAE_fp32.safetensors"
 
 echo "Model downloads complete!"
